87dfc662361beca6fbf7c26ae5d45e08798bc837
loujulien <laetitia_julien@live.fr>
2019-01-22 16:46:09 +0100

    security.yaml : add firewall main provider

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -30,9 +30,11 @@ class SecurityController extends AbstractController
             $hash = $encoder->encodePassword($user, $user->getPassword());
 
             $user->setPassword($hash);
-            
+
             $manager->persist($user);
             $manager->flush();
+
+            return $this->redirectToRoute('security_login');
         }
 
         return $this->render('security/registration.html.twig',[
@@ -42,15 +44,10 @@ class SecurityController extends AbstractController
 
 
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/connexion", name="security_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login()  
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig');
     }
 }
