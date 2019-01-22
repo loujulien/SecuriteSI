bfade3017c67eb2d082834a8ae4188019d1613b8
loujulien <laetitia_julien@live.fr>
2019-01-22 15:45:36 +0100

    template : add registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,9 +6,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Form\RegistrationType;
+use App\Entity\User;
+
 
 class SecurityController extends AbstractController
 {
+    /**
+     * @Route("/inscription", name="security_registration")
+     */
+    public function registration(Request $request, ObjectManager $manager)
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&&$form->isValid()){
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        return $this->render('security/registration.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+
     /**
      * @Route("/login", name="app_login")
      */
